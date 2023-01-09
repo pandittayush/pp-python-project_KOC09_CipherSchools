98d48821639d0f9368d5e24390db2a08a2118ad3
Ayush Pandey <119067526+pandittayush@users.noreply.github.com>
2023-01-09 18:23:18 +0530

    Update project1.py

--- project1.py
+++ project1.py
@@ -4,20 +4,18 @@ Cut the cake into N equal pieces.
 Cut the cake into N pieces of any size.
 Cut the cake into N pieces such that no two of them are equal'''
 
-
-
-for _ in range(int(input())):
-    n = int(input())
-    if 360%n == 0:
-        a1 = 'y' 
-    else:
-        a1 = 'n' 
-    if n <=360:
-        a2 = 'y'
-    else:
-        a2 = 'n' 
-    if n <= 26:
-        a3 = 'y'
-    else:
-        a3 = 'n' 
-    print(a1,a2,a3)
+    
+n = int(input()) #giving the integer input in which we want to divide the peices of cake!
+if 360%n == 0:
+    a1 = 'y' 
+else:
+    a1 = 'n' 
+if n <=360:
+    a2 = 'y'
+else:
+    a2 = 'n' 
+if n <= 26:
+    a3 = 'y'
+else:
+    a3 = 'n' 
+print(a1,a2,a3)
